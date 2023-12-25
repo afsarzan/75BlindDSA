51e60b5f1ea3efcaa0ced687f800e3ffde9e58ba
Afsar <afsar_zan@yahoo.com>
2023-12-26 00:25:29 +0530

    Update 7_min_sorted_array.js

--- 7_min_sorted_array.js
+++ 7_min_sorted_array.js
@@ -8,3 +8,27 @@ const findMin = (nums) => {
     }
     return nums[l];
   };
+
+  var findMin2 = function(nums) {
+    // let min=nums[0]
+    // nums.forEach(item=> {
+    //     min = Math.min(min,item)
+    // })
+
+    // return min;
+
+    // return Math.min(...nums)
+
+    let left =0, right = nums.length-1;
+    while(left<right ) {
+        let midPoint = Math.floor((left+right)/2);
+        if(nums[midPoint] > nums[right]) {
+            left = midPoint+1;
+        } else {
+            right = midPoint
+        }
+    }
+    return nums[left]
+};
+
+// explanation  = https://www.youtube.com/watch?v=dJC4uHyy2nc
