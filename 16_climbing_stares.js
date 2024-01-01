e7c4e23131084766121d7a3b927bfe228de0db4c
Afsar <afsar_zan@yahoo.com>
2024-01-01 14:31:54 +0530

    Update 16_climbing_stares.js

--- 16_climbing_stares.js
+++ 16_climbing_stares.js
@@ -19,3 +19,31 @@ var climbStairs = function(n) {
 
 
 // explanation https://www.youtube.com/watch?v=UUaMrNOvSqg
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    
+    if (n < 2) {
+        return 1;
+    }
+    
+    let firstStep = 1;
+    
+    let secondStep = 1;
+    
+    let thirdStep = 0;
+    
+    for (let i = 2; i <= n; i++) {
+        
+        thirdStep = firstStep + secondStep;
+        
+        firstStep = secondStep;
+        
+        secondStep = thirdStep;
+    }
+    return thirdStep;
+};
