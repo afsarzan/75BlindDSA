c3289bb638befe12515782f3ae7e0c44ca845b3d
afsarzan <afsar_zan@yahoo.com>
2025-09-28 19:35:28 -0700

    Js Implementation

--- 1089._Duplicate_Zeros.java
+++ 1089._Duplicate_Zeros.java
@@ -15,3 +15,26 @@ class Solution {
         }
     }
 }
+
+
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function (arr) {
+    // arr.forEach((num,index) => {
+    //     if(num == 0){
+    //         arr[index+1] = 0
+    //     }
+    // })
+
+    //[1,0,2,3,0,4,5,0]
+ for (let i = arr.length - 1; i >= 0;  i--) {
+           
+            if (arr[i] === 0) {
+                for (let j = arr.length-1; j > i; j--) {
+                    arr[j] = arr[j - 1]
+                }
+            }
+        }
+};
