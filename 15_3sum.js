7e0a9c5fe5c90c868a087620011f102814d7f1ad
afsarzan <afsar_zan@yahoo.com>
2025-09-29 12:31:54 -0700

    Create 15_3sum.js

---
+++ 15_3sum.js
@@ -0,0 +1,28 @@
+var threeSum = function(nums) {
+    let ans = [];
+    if(nums.length<3) return ans;
+    nums.sort( (a,b) => a-b);
+    
+    for(let i = 0; i<nums.length; i++) {
+        if(nums[i]>0) break;
+        if(i>0 && nums[i] === nums[i-1]) continue;
+        let start = i+1, end = nums.length-1;        
+        while ( start < end ) {             
+        const sum = nums[i] + nums[start] + nums[end];            
+        if(sum === 0) {
+            ans.push([nums[i],nums[start],nums[end]]);            
+            start++,end--;            
+            while(start < end && nums[start] == nums[start-1]) start++;
+            while(start < end && nums[end] == nums[end+1]) end--;
+          } else if(sum<0) {
+              start++;
+          } else if ( sum> 0) {
+              end--;
+          }
+        }
+    }
+    
+    return ans;
+    
+    
+};
