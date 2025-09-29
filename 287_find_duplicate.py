7ed89a99082f542bf367b76cd53e4af87337af48
afsarzan <afsar_zan@yahoo.com>
2025-09-28 18:26:34 -0700

    Update 287_find_duplicate.py

--- 287_find_duplicate.py
+++ 287_find_duplicate.py
@@ -18,3 +18,43 @@ class Solution:
             if(arr[i]==-1):
                 return i
             arr[i] = -1
+
+            /**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+
+    // // using set but it takes extra space 
+    // let dup = new Set();
+    // let dn = 0;
+    // for( let i = 0; i<nums.length; i++){
+    //     if(dup.has(nums[i])){
+    //         dn = nums[i];
+    //         break;
+    //     } else {
+    //         dup.add(nums[i]);
+    //     }
+    // }
+    // return dn
+
+    let slow = nums[0];
+    let fast = nums[0];
+    
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast);
+
+    console.log(slow)
+    console.log(fast)
+    
+    slow = nums[0];
+    while (slow !== fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    
+    return slow;
+    
+};
