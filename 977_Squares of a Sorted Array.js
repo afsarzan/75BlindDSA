ff44fd2d197c3cc45538081761db807a724e8ed8
afsarzan <afsar_zan@yahoo.com>
2025-09-29 11:29:39 -0700

    using two pointer solution with no extra space

--- 977_Squares of a Sorted Array.js
+++ 977_Squares of a Sorted Array.js
@@ -6,3 +6,27 @@ var sortedSquares = function(nums) {
     // console.log(nums.map(num => num*num))
     return nums.map(num => num*num).sort((a, b) => a - b)
 };
+
+
+ // using two pointer solution with no extra space
+
+    let n = nums.length;
+    let l = 0, r = n-1;
+    let res = new Array(n);
+    let pos = n-1;
+
+    while (l<=r){
+        let ls = nums[l] * nums[l];
+        let rs = nums[r] * nums[r];
+        if( ls > rs ){
+            res[pos] = ls;
+            l++;
+        } else {
+            res[pos] = rs;
+            r--;
+        }
+
+        pos--;
+        
+    }
+    return res;
