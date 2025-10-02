b53cdb4157ad783870a0228241f61716961520f6
afsarzan <afsar_zan@yahoo.com>
2025-10-01 18:59:04 -0700

    Update 876. Middle of the Linked List.js

--- 876. Middle of the Linked List.js
+++ 876. Middle of the Linked List.js
@@ -34,3 +34,30 @@ var middleNode = function(head) {
     }
     return res.next
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+    let slow = head;
+    let fast = head;
+    if(slow.next===null) return slow
+
+    while (slow && fast) {
+        slow = slow.next;
+        fast = fast?.next?.next;
+
+        if (fast?.next === null) break;
+    }
+
+    return slow
+
+};
