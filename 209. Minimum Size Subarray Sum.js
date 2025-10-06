4fc8c56e5b6632de55a73bdea4f42cb975566624
afsarzan <afsar_zan@yahoo.com>
2025-10-05 17:51:43 -0700

    Create 209. Minimum Size Subarray Sum.js

---
+++ 209. Minimum Size Subarray Sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    
+    let min = Number.MAX_SAFE_INTEGER;
+        const initSum = nums.reduce( (a,b) => a+b,0);
+        // console.log({initSum})
+       if( initSum <target) return 0; 
+    for( let i= 0; i< nums.length; i++){
+        const first = nums[i];
+        let part = nums.slice(i,nums.length);
+        let sum = part.reduce( (a,s) => a+s,0);
+        if(sum>=target){
+            min = Math.min(min,part.length)
+        // console.log({part,sum,len:part.length,min})
+        }
+    }
+        return min;
+};
