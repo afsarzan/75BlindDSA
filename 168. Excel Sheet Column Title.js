2da338fdc8d73471d3bec78bf474c58c1e1ce55d
afsarzan <afsar_zan@yahoo.com>
2025-10-12 00:42:35 -0700

    Create 168. Excel Sheet Column Title.js

---
+++ 168. Excel Sheet Column Title.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+     let res = "";
+
+    while (columnNumber > 0) {
+        columnNumber--;
+        console.log({res})
+        res = String.fromCharCode((columnNumber % 26) + "A".charCodeAt(0)) + res;
+        console.log({res})
+        columnNumber = Math.floor(columnNumber / 26);
+    }
+    
+    return res;   
+};
