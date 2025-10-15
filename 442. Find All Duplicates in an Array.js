ea46dd24e7c2ab6927c3a2cf43d5316ca6d0100a
afsarzan <afsar_zan@yahoo.com>
2025-10-14 21:44:10 -0700

    Refactor findDuplicates function to remove unused code and improve clarity

--- 442. Find All Duplicates in an Array.js
+++ 442. Find All Duplicates in an Array.js
@@ -3,25 +3,25 @@
  * @return {number[]}
  */
 var findDuplicates = function (nums) {
-    let res = [];
-    let map = new Map();
-    for(let i = 0; i < nums.length; i++) {
-        map.set(nums[i]+'t',(map.get(nums[i]+'t')||0)+1);
-        if(map.get(nums[i]+'t')>1) {
-            res.push(nums[i])
+    // let res = [];
+    // let map = new Map();
+    // for(let i = 0; i < nums.length; i++) {
+    //     map.set(nums[i]+'t',(map.get(nums[i]+'t')||0)+1);
+    //     if(map.get(nums[i]+'t')>1) {
+    //         res.push(nums[i])
+    //     }
+    // }   
+    // // console.log({res})
+    // return res;
+
+
+ let output = []
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[Math.abs(nums[i]) - 1] < 0) {
+            output.push(Math.abs(nums[i]))
+        } else {
+            nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1]
         }
-    }   
-    // console.log({res})
-    return res;
-
-
-//  let output = []
-//     for (let i = 0; i < nums.length; i++) {
-//         if (nums[Math.abs(nums[i]) - 1] < 0) {
-//             output.push(Math.abs(nums[i]))
-//         } else {
-//             nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1]
-//         }
-//     }
-//     return output
+    }
+    return output
 };
