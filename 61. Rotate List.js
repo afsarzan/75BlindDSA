e677aaa1e25dcb54a67ad9929882f1738ed1399a
afsarzan <afsar_zan@yahoo.com>
2025-10-19 13:51:10 -0700

    Refactor rotateRight function to improve efficiency and readability by eliminating unnecessary variables and logic

--- 61. Rotate List.js
+++ 61. Rotate List.js
@@ -10,43 +10,72 @@
  * @param {number} k
  * @return {ListNode}
  */
-var rotateRight = function (head, k) {
-    if(head == null) return null;
-    let next = ListNode(0);
-    let res = head;
-    let totalNodes = 0;
-    while (head) {
-        totalNodes++;
-        head = head.next;
-    }
 
-    if( k == totalNodes || totalNodes == 1) return res;
-    head = res;
-    // let result = ListNode(0);
-    for(let i = 0; i< k%totalNodes; i++ ){
-        head = rotate(head,totalNodes)
-    }
-    // const ro = rotate(head,totalNodes)
-    // console.log({ head });
-    return head;
 
-};
+ var rotateRight = function (head, k) {
+    if (!head || k === 0) return head;
 
-function rotate(head, totalNodes) {
-    const init = head;
-
-    let counter = 1;
-    while (head) {
-        console.log({ counter })
-        if (counter == totalNodes - 1) {
-            const next = head.next;
-            head.next = null;
-            next.next = init;
-            // console.log({ next })
-            return next;
+        let length = 1;
+        let tail = head;
+        while (tail.next) {
+            tail = tail.next;
+            length++;
         }
-        counter++;
-        head = head.next;
-    }
-    return head;
-}
+
+        k = k % length;
+        if (k === 0) return head;
+
+         tail.next = head; // form a cycle
+         let stepsToNewHead = length - k;
+         let newTail = tail;
+        while (stepsToNewHead--) {
+            newTail = newTail.next;
+        }
+
+        let newHead = newTail.next;
+        newTail.next = null;
+        console.log({ newHead });
+        return newHead;
+ }
+
+
+// var rotateRight = function (head, k) {
+//     if (!head || k === 0) return head;
+//     let next = ListNode(0);
+//     let res = head;
+//     let totalNodes = 0;
+//     while (head) {
+//         totalNodes++;
+//         head = head.next;
+//     }
+
+//     if( k == totalNodes || totalNodes == 1) return res;
+//     head = res;
+//     // let result = ListNode(0);
+//     for(let i = 0; i< k%totalNodes; i++ ){
+//         head = rotate(head,totalNodes)
+//     }
+//     // const ro = rotate(head,totalNodes)
+//     // console.log({ head });
+//     return head;
+
+// };
+
+// function rotate(head, totalNodes) {
+//     const init = head;
+
+//     let counter = 1;
+//     while (head) {
+//         console.log({ counter })
+//         if (counter == totalNodes - 1) {
+//             const next = head.next;
+//             head.next = null;
+//             next.next = init;
+//             // console.log({ next })
+//             return next;
+//         }
+//         counter++;
+//         head = head.next;
+//     }
+//     return head;
+// }
